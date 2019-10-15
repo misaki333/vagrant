7f407a37e370fb29a544ac859e64d6a608854468
misaki333 <misakiumenishi@gmail.com>
2019-10-15 15:43:38 +0900

    [FINISH]4-8

---
+++ bookers/app/controllers/books_controller.rb
@@ -0,0 +1,45 @@
+class BooksController < ApplicationController
+
+  def create
+  	@book = Book.new(book_params)
+  	if @book.save
+  		flash[:notice] = "Book was successfully updated."
+  		redirect_to book_path(@book.id)
+  	else
+  		@books = Book.all
+  		render action: :index
+  	end
+  end
+
+  def index
+  	@books = Book.all
+  	@book = Book.new
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	flash[:notice] = "Book was successfully updated."
+  	book = Book.find(params[:id])
+  	book.update(book_params)
+  	redirect_to book_path(book.id)
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_path
+  end
+
+  private
+
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
