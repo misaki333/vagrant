4ea63518da5e363252a8f985635d01b9adb08957
misaki333 <misakiumenishi@gmail.com>
2019-10-15 15:39:31 +0900

    [FINISH]3-9

---
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'e48ead6e1ff0fdfc4a9687e7befe873305fc4bf7e5e1fd75d4547de5653e4e75abcd8914f98a3af0c922fc2e071c1cca2f2f412d93d1b02b329bfe058c2a1697'
