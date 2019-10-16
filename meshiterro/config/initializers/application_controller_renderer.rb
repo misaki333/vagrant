b5fca3a42e842724974218551f2d3b10507d3d8c
misaki333 <misakiumenishi@gmail.com>
2019-10-16 19:01:24 +0900

    [FINISH]5-4

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8746a57eb947992f8ecc5f73bcf47ce058fb6a4b87de49ede2b475e22cfba36daf6308902d03888153c6759e361e8d38aeff7e121cb5671dc3d26b51c340d750'
